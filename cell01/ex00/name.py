43d262568d942a527b4e51af55fa5d19f2d9cf52
oraya-chai <oraya.chaipanya2@gmail.com>
2026-02-11 10:08:19 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Oraya"
+last_name = "Chaipanya"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Oraya"
+last_name = "Chaipanya"
+whole_name = first_name + " " + last_name
+print(whole_name)
+
