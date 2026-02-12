1b933582963134f2a277ccda581054b4fed47220
oraya-chai <oraya.chaipanya2@gmail.com>
2026-02-12 16:22:34 +0700

    Add files via upload

---
+++ miniproject/checkmate.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+def checkmate(board_str=None):
+
+    # 1 กรณีที่ไม่มีinput
+    if board_str is None: 
+        print("Error")
+        return
+
+    # 2 ต้องเป็นstringเท่านั้น เผื่อมีการใส่เลข
+    if not isinstance(board_str, str):
+        print("Error")
+        return 
+    
+    rows = board_str.splitlines() 
+    # ['', 'R...', '.K..', '..P.', '....'] โดย '' มาจาก newlineตัวแรกในtripple quoteเพราะมีการกดขึ้นบรรทัดใหม่เลยเป็นงั้น
+    # ไม่มีที่ตัวสุดท้ายเพราะ splitlines() ไม่สร้างสมาชิกว่างจากnewlineตัวสุดท้าย
+    rows = [row for row in rows if row.strip() != ""]
+    # ['R...', '.K..', '..P.', '....']
+    
+    # 3 กรณีกระดานว่าง แบบ board = " "
+    if len(rows) == 0:
+        print("Error") 
+        return
+
+    # 4 เช็กว่าเป็นสี่เหลี่ยมจตุรัสมั้ย
+    n = len(rows)
+    for row in rows:
+        if len(row) != n:
+            print("Error")
+            return
+        
+    # 5 หา King
+    king_position = None # ไว้เก็บตำแหน่งคิงในรูปของ(row, col)เริ่มต้นเป็นnoneเพราะยังไม่เจอking
+    king_count = 0 # นับจำนวนคิงที่เจอมีกี่ตัว
+    # n ตัวบอกว่าจตุรัสเท่าไร / n=4 -> 0 1 2 3
+    # i = แถว
+    # j = column
+    for i in range(n):
+        for j in range(n):
+            if rows[i][j] == "K":
+                king_position = (i, j)
+                king_count += 1
+    if king_count > 1:
+        print("Error")
+        return 
+    elif king_count == 0:
+        print("Error")
+        return
+    king_row, king_col = king_position
+
+    # 5 ฟังก์ชันช่วยเช็คขอบกระดาน
+    def in_bounds(ro, co):
+        return 0 <= ro < n and 0 <= co < n
+
+    # 6 ตรวจหมากทุกตัว
+    for i in range(n):
+        for j in range(n):
+            piece = rows[i][j]
+            # คัดเฉพาะหมากที่โจมตีได้ ถ้าไม่ใช่หมากที่โจมตีได้ ก็ให้ข้ามไปช่องถัดไป
+            if piece not in ("R", "B", "Q", "P"):
+                continue
+
+            # ROOK
+            if piece == "R":
+                directions = [(1,0), (-1,0), (0,1), (0,-1)]
+                # ลง (1,0) row + 1, col +0
+                # ขึ้น (-1,0) row -1, col +0
+                # ขวา (0,1) row +0, col +1
+                # ซ้าย (0,-1) row +0, col -1
+
+            # BISHOP
+            elif piece == "B":
+                directions = [(1,1), (1,-1), (-1,1), (-1,-1)]
+                # ขวาล่าง (1,1) row +1, col +1
+                # ซ้ายล่าง (1,-1) row +1, col -1
+                # ขวาบน (-1,1) row -1, col 1
+                # ซ้ายบน (-1,-1) row -1, col -1
+            
+            # QUEEN
+            elif piece == "Q":
+                directions = [
+                    (1,0), (-1,0), (0,1), (0,-1),
+                    (1,1), (1,-1), (-1,1), (-1,-1)]
+                # Queen = R + B
+
+            # PAWN
+            elif piece == "P":
+                pawn_attacks = [(-1, -1), (-1, 1)]
+                # ซ้ายบน (-1,-1) row -1, col-1
+                # ขวาบน (-1,1) row -1, col 1
+
+                # ตรวจเส้นทางเดินpawn -> ให้มีการเช็กทั้ง2ฝั่ง 
+                for dr, dc in pawn_attacks:
+                    # ซ้ายบน dr = -1, dc = -1
+                    # ขวาบน dr = -1, dc = 1 
+                    # มาคำนวณช่องที่ pawn โจมตีได้
+                    ro = i + dr 
+                    co = j + dc
+                    if in_bounds(ro, co) and (ro, co) == (king_row, king_col):
+                        # เช็คว่ายังอยู่ในกระดาน
+                        # เช็คช่องที่ Pawn โจมตี = ช่องที่ King อยู่
+                        # ถ้าใช้ king โดนโจมตี
+                        print("Success")
+                        return
+                # ตรวจpawn เสร็จแล้ว ข้ามไปดูช่องถัดไปในกระดาน
+                continue
+            # ตรวจเส้นทางของ R/B/Q
+            for dr, dc in directions:
+                ro, co = i + dr, j + dc
+
+                # ยังไม่หลุดกระดานก็ให้เดินต่อไปเรื่อยๆ
+                while in_bounds(ro, co):
+                    if rows[ro][co] == "K":
+                        # คิงโดนโจมตี)
+                        print("Success")
+                        return
+                    # เจอหมากตัวอื่นให้หยุด -> หมากกินข้ามตัวอื่นไม่ได้
+                    elif rows[ro][co] in ("R", "B", "Q", "P"):
+                        break
+                    # เลื่อนไปอีก1ช่องในทิศทางเดิม จนกว่าจะหลุดขอบ
+                    ro += dr
+                    co += dc
+    # คิงไม่โดนโจมตี
+    print("Fail")
+    return
+
+
+
